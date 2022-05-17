5ecfe7894773ab3030489f79f78e762036c89f8a
Adek728 <105450762+Adek728@users.noreply.github.com>
2022-05-17 19:52:34 +0200

    Delete zad4 directory

--- zad4/Admin/Main.java
+++
@@ -1,9 +0,0 @@
-package zad1.Admin;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(GUIAdmin.class);
-    }
-}
